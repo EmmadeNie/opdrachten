31709b4741fe9124217d7c4f552f16cc79553080
EmmadeNie <64635800+EmmadeNie@users.noreply.github.com>
2020-05-05 10:19:26 +0200

    Add files via upload

---
+++ week 1/Opdracht gebruik je eerste Javascript/externalscript.js
@@ -0,0 +1,17 @@
+let name = 'Emma';
+let age = 28;
+let number = 2;
+let ageSpelledOut = 'twenty eight';
+
+
+console.log(name)
+
+console.log(typeof age)
+
+console.log(age + number)
+
+console.log(typeof ageSpelledOut)
+
+
+
+2 + 2
