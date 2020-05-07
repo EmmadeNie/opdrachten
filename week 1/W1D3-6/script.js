226b8868898cb34ceeec9d82ecca6d8b45033e43
EmmadeNie <64635800+EmmadeNie@users.noreply.github.com>
2020-05-07 08:20:13 +0200

    Update script.js

--- week 1/W1D3-6/script.js
+++ week 1/W1D3-6/script.js
@@ -1,21 +1,24 @@
 let name = prompt("Hi there! What is your name?");
 alert("Hi, " + name + "!")
+let range = +prompt(name + ", till what number would you like to guess?")
+let beurt = +prompt(name + ", how many tries do you give yourself?")
+let guess = +prompt(`Oke, ${name}. Guess a number between 0 and ${range}. You have ${beurt} tries.`)
 
-let guess = +prompt("guess a number (van 0 tm 25" + name)
 
-let beurt = 4
-
-if (guess > 25) {
-    guess = prompt("Guess again. 0 tm 25 please");
+if (guess > range) {
+    guess = prompt("Guess again. You gave yourself a range between 0 and " + range);
 }
+range++
+let random = Math.floor(Math.random() * range)
 
-console.log(guess)
-console.log(typeof guess)
+alert(random)
 
-let random = Math.floor(Math.random() * 2)
+while (beurt > 0) {
+    if (guess === random) {
+        alert("You got it girl!")
+    } else {
+        guess = prompt(`Wrong! Guess again. je hebt nog ${beurt} beurt(en) over.`);
+        beurt--;
+    }
+} alert("Helaas... Je beurten zijn voorbij! ")
 
-while (guess !== random || beurt > 0) {
-    guess = prompt(`Wrong! Guess again. je hebt nog ${beurt} beurt(en) over.`);
-    beurt--;
-}
-alert("You got it girl!")
